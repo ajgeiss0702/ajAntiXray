3b9130fbdada78e9e2da47a0e7567f4e22a2087b
jarrett-workman <thowanwhitewolf@gmail.com>
2025-12-30 20:57:06 -0700

    Updated gradle, plugin.yml and commands to safe spot, still can't built

--- src/main/java/us/ajg0702/antixray/Commands.java
+++ src/main/java/us/ajg0702/antixray/Commands.java
@@ -2,8 +2,6 @@ package us.ajg0702.antixray;
 
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
-import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -16,23 +14,103 @@ import java.util.Map;
 public class Commands implements CommandExecutor {
     private final Main plugin;
 
-    private final LegacyComponentSerializer legacyComponentSerializer = LegacyComponentSerializer.legacySection();
-
     public Commands(Main plugin) {
         this.plugin = plugin;
     }
 
+    @Override
     public boolean onCommand(CommandSender bsender, Command command, String label, String[] args) {
-        Audience sender = plugin.adventure().sender(bsender);
-        if(label.equalsIgnoreCase("ajecho")) {
-            StringBuilder message = new StringBuilder();
-            for(String arg : args) {
-                message.append(arg).append(" ");
+        final Audience sender = plugin.adventure().sender(bsender);
+
+        //Get command name and check if it has 0 arguments, echo back the command
+        if (command.getName().equalsIgnoreCase("ajecho")){
+            if (args.length == 0){
+                sender.sendMessage(plugin.getMessages().getComponent("cmd-syntax","CMD:", label));
+                return true;
             }
-            sender.sendMessage(plugin.getMessages().toComponent(Messages.color(message.toString())));
+            String message = String.join(" ", args);
+            sender.sendMessage(plugin.getMessages().toComponent(Messages.color(message)));
             return true;
         }
+
         if(args.length == 1) {
+
+            // ajaxr reload
+            if (args[0].equalsIgnoreCase("reload"))
+            {
+                if (!besender.hasPermission("ajaxr.reload"))
+                {
+                    sender.sendMessage(plugin.getMessages().getComponent("noperm"));
+                    return true;
+                }
+                plugin.reloadConfig();
+                plugin.getMessage().reload();
+                sender.sendMessage(plugin.getMessages().getComponent("config-reloaded"));
+                return true;
+            }
+
+            // ajaxr check <player>
+            if (!bsender.hasPermission("ajaxr.check")) {
+                sender.sendMessage(plugin.getMessages().getComponent("noperm"));
+                return true;
+            }
+
+            //the player we are checking MUST be online
+            Player p = Bukkit.getPlayerExact(args[0]); // exact match player name to avoid surprises
+            if (p == null) {
+                sender.sendMessage(plugin.getMessages().getComponent("player-not-found", "PLAYER:", args[0]));
+                return true;
+            }
+
+            //Start building the message header
+            Component out = plugin.getMessages().getComponent("get.header", "PLAYER:", p.getName())
+                    .append(Component.newline());
+
+            //Get blocks that we are tracking
+            Map<String, Integer> bks = plugin.getBlocks(p.getUniqueId());
+
+            for (Map.Entry<String, Integer> entry : bks.entrySet()) {
+                String block = entry.getKey();
+                int blocknum = entry.getValue();
+
+                int blockmax = plugin.warnBlocks.getOrDefault(block, 0);
+                if (blockmax <= 0) continue;
+
+                String countcolor;
+                if (blocknum >= blockmax) {
+                    countcolor = (blocknum > blockmax + (int) (blockmax * 0.25)) ? "<dark_red>" : "<red>";
+                } else {
+                    if (blocknum > blockmax - (int) (blockmax * 0.2)) {
+                        countcolor = "<yellow>";
+                    } else if (blocknum > blockmax - (int) (blockmax * 0.35)) {
+                        countcolor = "<dark_green>";
+                    } else {
+                        countcolor = "<green>";
+                    }
+                }
+
+                out = out.append(plugin.getMessages().getComponent(
+                        "get.format",
+                        "BLOCK:", block,
+                        "COUNTCOLOR:", countcolor,
+                        "COUNT:", String.valueOf(blocknum),
+                        "DELAY:", String.valueOf(plugin.delay / 60000)
+                )).append(Component.newline());
+            }
+
+            //Display the report
+            sender.sendMessage(out);
+            return true;
+        }
+
+        //Display syntax/help
+        sender.sendMessage(plugin.getMessages().getComponent("cmd-syntax", "CMD:", label));
+        return true;
+    }
+}
+
+// Old Stuff
+            /*
             if(!args[0].equalsIgnoreCase("reload")) {
                 if(!bsender.hasPermission("ajaxr.check")) {
                     sender.sendMessage(plugin.getMessages().getComponent("noperm"));
@@ -95,3 +173,4 @@ public class Commands implements CommandExecutor {
         return true;
     }
 }
+*/
