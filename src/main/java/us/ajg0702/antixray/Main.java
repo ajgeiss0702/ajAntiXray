e015003d2285b8e6b333fc380d32ea2a7159db35
ajgeiss0702 <aj@ajg0702.us>
2026-06-14 12:01:41 -0700

    some small cleanup

--- src/main/java/us/ajg0702/antixray/Main.java
+++ src/main/java/us/ajg0702/antixray/Main.java
@@ -54,8 +54,11 @@ public class Main extends JavaPlugin {
 				i.remove();
 			} else {
 				String bk = player.get(t);
-				int before = bks.get(bk);
-				bks.put(bk, before+1);
+                bks.compute(bk, (k, before) ->
+						before == null
+								? 1
+								: before + 1
+				);
 			}
 		}
 		players.put(uuid, player);
@@ -224,62 +227,58 @@ public class Main extends JavaPlugin {
 			if(bks.get(bk) >= warnBlocks.get(bk)) {
 				if(recentNotifees.contains(player)) {
 					recentNotifees.remove(player);
-					//Bukkit.getLogger().info("[ajAntiXray] Skipping player " + player.getName());
 					break;
 				}
 				lastNotify.put(puuid, System.currentTimeMillis());
-				//Bukkit.getLogger().info("[ajAntiXray] "+i+"/"+(bks.keySet().size()-2));
-				Bukkit.getScheduler().runTaskLater(this, new Runnable(){
-					public void run() {
-						if(!recentNotifees.contains(player)) {
-							recentNotifees.add(player);
-						}
-						for(Player admin : Bukkit.getOnlinePlayers()) {
-							if(!admin.hasPermission("ajaxr.notify")) continue;
-							adventure.player(admin).sendMessage(messages.getComponent(
-									"notify.format",
-									"PLAYER:" + player.getName(),
-									"COUNT:" + bks.get(bk),
-									"ORE:" + bk,
-									"DELAY:" + (delay/60000)
-							));
-						}
-						if(!notifySound.equalsIgnoreCase("none")) {
-							for(Player p : Bukkit.getOnlinePlayers()) {
-								if(p.hasPermission("ajaxr.notify")) {
-									try {
-										Sound sound = Sound.valueOf(notifySound);
-										p.playSound(p.getLocation(), sound, 1f, 1f);
-									} catch(Exception e) {
-										Bukkit.getLogger().warning("[ajAntiXray] Could not find sound '"+notifySound+"'!");
-										break;
-									}
-								}
-							}
-						}
-						for(String command : commands) {
-							Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("\\{PLAYER}", player.getName())
-								.replaceAll("\\{COUNT}", bks.get(bk)+"")
-								.replaceAll("\\{ORE}", bk)
-								.replaceAll("\\{DELAY}", (delay/60000)+"")
-								);
-						}
+				Bukkit.getScheduler().runTaskLater(this, () -> {
+                    if(!recentNotifees.contains(player)) {
+                        recentNotifees.add(player);
+                    }
+                    for(Player admin : Bukkit.getOnlinePlayers()) {
+                        if(!admin.hasPermission("ajaxr.notify")) continue;
+                        adventure.player(admin).sendMessage(messages.getComponent(
+                                "notify.format",
+                                "PLAYER:" + player.getName(),
+                                "COUNT:" + bks.get(bk),
+                                "ORE:" + bk,
+                                "DELAY:" + (delay/60000)
+                        ));
+                    }
+                    if(!notifySound.equalsIgnoreCase("none")) {
+                        for(Player p : Bukkit.getOnlinePlayers()) {
+                            if(p.hasPermission("ajaxr.notify")) {
+                                try {
+                                    Sound sound = Sound.valueOf(notifySound);
+                                    p.playSound(p.getLocation(), sound, 1f, 1f);
+                                } catch(Exception e) {
+                                    Bukkit.getLogger().warning("[ajAntiXray] Could not find sound '"+notifySound+"'!");
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    for(String command : commands) {
+                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replaceAll("\\{PLAYER}", player.getName())
+                            .replaceAll("\\{COUNT}", bks.get(bk)+"")
+                            .replaceAll("\\{ORE}", bk)
+                            .replaceAll("\\{DELAY}", (delay/60000)+"")
+                            );
+                    }
 
-						String webhookUrl = config.getString("discord-webhook");
-						if(!webhookUrl.isEmpty()) {
-							String webhookMessage = messages.getString(
-									"webhook.format",
-									"PLAYER:" + player.getName(),
-									"COUNT:" + bks.get(bk),
-									"ORE:" + bk,
-									"DELAY:" + (delay/60000)
-							);
-							Bukkit.getScheduler().runTaskAsynchronously(Main.this,
-									() -> WebhookSender.send(getLogger(), webhookUrl, webhookMessage)
-							);
-						}
-					}
-				}, (long) (Math.floor((Math.random()*2) * 20)));
+                    String webhookUrl = config.getString("discord-webhook");
+                    if(!webhookUrl.isEmpty()) {
+                        String webhookMessage = messages.getString(
+                                "webhook.format",
+                                "PLAYER:" + player.getName(),
+                                "COUNT:" + bks.get(bk),
+                                "ORE:" + bk,
+                                "DELAY:" + (delay/60000)
+                        );
+                        Bukkit.getScheduler().runTaskAsynchronously(Main.this,
+                                () -> WebhookSender.send(getLogger(), webhookUrl, webhookMessage)
+                        );
+                    }
+                }, (long) (Math.floor((Math.random()*2) * 20)));
 			}
 		}
 	}
@@ -289,10 +288,6 @@ public class Main extends JavaPlugin {
 			notifyAdmins(Bukkit.getPlayer(puuid));
 		}
 	}
-	
-	
-	@SuppressWarnings("unlikely-arg-type")
-
 
 	public BukkitAudiences adventure() {
 		if(this.adventure == null) {
